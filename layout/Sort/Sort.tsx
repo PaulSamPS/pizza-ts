2e3b96c33da59161a0eec23c803da7b1a05d42bf
Pavel Samoylenko <psamoylenko2@gmail.com>
2021-10-20 06:37:58 +0300

    modal

--- layout/Sort/Sort.tsx
+++ layout/Sort/Sort.tsx
@@ -2,14 +2,21 @@ import Arrow from './arrow.svg'
 import styles from './Sort.module.scss'
 import {SortProps} from "./Sort.props";
 import cn from 'classnames'
+import {useState} from "react";
 
 export const Sort = ({className, ...props}: SortProps): JSX.Element => {
+    const [showModal, setShowModal] = useState<boolean>(false)
+
     return (
         <div className={cn(styles.sort, className)} {...props}>
             <Arrow className={styles.arrow}/>
             <p className={styles.sortTitle}>Сортироква по:</p>
-            <p className={styles.selected}>популярности</p>
-            <ul className={styles.modal}>
+            <p className={styles.selected} onClick={() => setShowModal(!showModal)}>
+                популярности
+            </p>
+            <ul className={cn(styles.modal, {
+                [styles.showModal]: showModal
+            })}>
                 <li>популярности</li>
                 <li>цене</li>
                 <li>алфавиту</li>
